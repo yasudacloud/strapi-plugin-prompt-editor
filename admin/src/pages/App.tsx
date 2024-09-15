54d8eaf9ac6ae80a9e7641c6ff813b2711b76e35
yasudacloud <hello@yasuda.cloud>
2024-09-16 02:51:15 +0900

    Modified for Strapi v4

--- admin/src/pages/App.tsx
+++ admin/src/pages/App.tsx
@@ -1,13 +1,13 @@
 import {Page} from '@strapi/design-system';
-import {Routes, Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Setting from "./Setting";
 
 const App = () => {
   return (
-    <Routes>
-      <Route index path={`/settings/prompt-editor`} element={<Setting/>}/>
-      <Route path="*" element={<Page.Error/>}/>
-    </Routes>
+    <Switch>
+      <Route path={`/settings/prompt-editor`} component={Setting}/>
+      <Route path="*" component={Page.Error}/>
+    </Switch>
   );
 };
 
